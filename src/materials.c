eb414095d99a228fa86c547a2f59a91b7072c8b2
Thomas <thomasdamiani13@gmail.com>
2018-01-26 17:35:16 +0100

    add materials

---
+++ src/materials.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "materials.h"
+
+#define DEFAULT_K_A 0.5
+#define DEFAULT_K_D 0.5
+#define DEFAULT_K_S 0.5
+#define DEFAULT_ALPHA 10
+
+Material MATERIAL_create_material(double k_a, double k_d, double k_s, double alpha){
+  Material material;
+
+  material.k_a = k_a;
+  material.k_d = k_d;
+  material.k_s = k_s;
+  material.alpha = alpha;
+  
+  return material;
+}
+
+Material MATERIAL_create_default(){
+  return MATERIAL_create_material(DEFAULT_K_A, DEFAULT_K_D, DEFAULT_K_S, DEFAULT_ALPHA);
+}
---
+++ src/materials.h
@@ -0,0 +1,15 @@
+#ifndef MATERIALS_H
+#define MATERIALS_H
+
+  typedef struct Material Material;
+  struct Material{
+    double k_a;//ambiant component, between 0 and 1
+    double k_d;//diffuse component, between 0 and 1
+    double k_s;//specular component, between 0 and 1
+    double alpha;//material shine, more than 1. The larger it is, the shinest the surface is.
+  };
+
+  Material MATERIAL_create_material(double k_a, double k_d, double k_s, double alpha);
+  Material MATERIAL_create_default();
+
+#endif
